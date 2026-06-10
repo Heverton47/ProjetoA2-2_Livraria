cb0559d386e63865c2123b26d183d5dc674735ea
Caio Lamers <caiolamers@gmail.com>
2026-06-10 12:29:33 -0300

    Add simple SQLite persistence

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ProjetoA2-2_Livraria
-Trabalho de C# dando continuidade ao primeiro projeto da livraria.
+
---
+++ livrariaApi/Migrations/20260609225659_CriarBanco.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace livrariaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrinhos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Cliente = table.Column<string>(type: "TEXT", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinhos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Autor = table.Column<string>(type: "TEXT", nullable: false),
+                    Genero = table.Column<string>(type: "TEXT", nullable: false),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CarrinhoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Livros_Carrinhos_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinhos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_CarrinhoId",
+                table: "Livros",
+                column: "CarrinhoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Carrinhos");
+        }
+    }
+}
